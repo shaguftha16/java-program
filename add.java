48fb446dd84432cf3c80577043f05cd3829a4543
shaguftha16 <shaguftha4324@gmail.com>
2026-06-30 19:19:31 +0530

    added new program

---
+++ add.java
@@ -0,0 +1,23 @@
+public class Pratice {
+    public static void main(String[] args) {
+ System.out.println("adding using operations and datatypes");
+
+ int a=10;
+ double b= 15.5;
+ double sum= a+b;
+ short c=10;
+ long n=22;
+  long m = c + n;
+
+        int h =10;
+        double i= 15.5;
+        double sum1= h-i;
+        short t=10;
+        long u=22;
+        long m2 = t * u;
+
+        System.out.println ("the sum and substraction is: \n" + "sum:" +sum + "\n"+
+                "m:" +m + "\n" +"sum1:" +sum1 + "\n" +"sum:" +sum + "\n");
+
+    }
+}
