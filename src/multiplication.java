776a78e157906ffefe5c7cd4d345c317905c6eed
shaguftha16 <shaguftha4324@gmail.com>
2026-07-02 15:52:22 +0530

    for loop added

--- src/multiplication.java
+++ src/multiplication.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        for(int i =1;i<=10;i++){
+            int result = num *i;
+            System.out.println(num + "*" + i + "=" +result);
+        }
+    }
+}
 
